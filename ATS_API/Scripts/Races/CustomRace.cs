c1649dfed1ad0b7cfbcfd5d96d76e63cbeabc01f
Tom <tom@txkgaming.com>
2024-05-22 17:27:35 +0200

    started adding custom RaceBuilder & everything needed for syncing

---
+++ ATS_API/Scripts/Races/CustomRace.cs
@@ -0,0 +1,15 @@
+﻿using ATS_API.Helpers;
+using Eremite.Model;
+
+namespace ATS_API.Scripts.Races;
+
+public class CustomRace : ASyncable <RaceModel>
+{
+    public RaceTypes id;
+    public RaceModel raceModel;
+
+    public override void Sync(RaceModel model)
+    {
+        //todo
+    }
+}
